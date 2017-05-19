3e8828e4d49e5990a7a40e45bdc3d5870f51f144
shivin9 <shivin.srivastava@gmail.com>
2017-05-19 08:06:22 +0530

    separated the operate function into two

--- src/operator.jl
+++ src/operator.jl
@@ -1,21 +1,27 @@
 include("fornberg.jl")
 
 
+function convolve!{T}(x_temp::SharedArray{T}, x::Vector{T}, coeffs::Array{T,1}, i::Int64, mid::Int64, wndw_low::Int64, wndw_high::Int64)
+    wndw_low = Int(max(1, low(i)))
+    wndw_high = Int(min(stencil_length, high(i)))
+    x_temp[i] = sum(coeffs[wndw_low:wndw_high] .* x[(i-(mid-wndw_low)):(i+(wndw_high-mid))])
+end
+
 function operate{T <: AbstractFloat}(fdg::FiniteDifferenceEvenGrid{T}, x::Vector{T})
     coeffs = fdg.stencil_coefs
     stencil_length = length(coeffs)
     mid = Int(ceil(stencil_length/2))
     boundary_point_count = stencil_length - mid
-    x_temp = zeros(x)
+    x_temp = SharedArray(T, length(x))
     L = length(x)
 
     low(i) = mid + (i-1)*(1-mid)/boundary_point_count
     high(i) = stencil_length - (stencil_length-mid)*(i-L+boundary_point_count)/(boundary_point_count)
 
-    for i in 1 : length(x)
+    @parallel for i in 1 : length(x)
         wndw_low = Int(max(1, low(i)))
         wndw_high = Int(min(stencil_length, high(i)))
-        x_temp[i] = sum(coeffs[wndw_low:wndw_high] .* x[(i-(mid-wndw_low)):(i+(wndw_high-mid))])
+        convolve!(x_temp, x, coeffs, i, mid, wndw_low, wndw_high)
     end
     return x_temp
 end
