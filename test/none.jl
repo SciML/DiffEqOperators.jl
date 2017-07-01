e47c057fa04aeafc0d116e40efb7790547550801
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-07-01 23:41:24 +0100

    Get correct order at end derivative

--- test/none.jl
+++ test/none.jl
@@ -1,3 +1,4 @@
+using FactCheck
 using Base.Test
 
 context("One-sided derivatives at boundaries: ") do
@@ -7,19 +8,31 @@ context("One-sided derivatives at boundaries: ") do
     A = LinearOperator{Float64}(d_order,approx_order,1.0,N,:None,:None)
     Amat = full(A)
 
-    @test Amat[1,1:3] == [-1.5, 2.0, -0.5]
+    @test isapprox(Amat[1,1:3], [-1.5, 2.0, -0.5])
     for row = 2:N-1
-        @test Amat[row,row-1:row+1] == [-0.5, 0.0, 0.5]
+        @test isapprox(Amat[row,row-1:row+1], [-0.5, 0.0, 0.5])
     end
-    @test Amat[end,end-2:end] == [0.5, -2.0, 1.5]
+    @test isapprox(Amat[end,end-2:end], [0.5, -2.0, 1.5])
 
     d_order = 2
     A = LinearOperator{Float64}(d_order,approx_order,1.0,N,:None,:None)
     Amat = full(A)
 
-    @test Amat[1,1:3] == [1.0, -2.0, 1.0]
+    @test isapprox(Amat[1,1:4], [2.0, -5.0, 4.0, -1.0])
     for row = 2:N-1
-        @test Amat[row,row-1:row+1] == [1.0, -2.0, 1.0]
+        @test isapprox(Amat[row,row-1:row+1], [1.0, -2.0, 1.0])
     end
-    @test Amat[end,end-2:end] == [1.0, -2.0, 1.0]
+    @test isapprox(Amat[end,end-3:end], [-1.0, 4.0, -5.0, 2.0])
+
+    N = 10
+    d_order = 1
+    approx_order = 3
+    A = LinearOperator{Float64}(d_order,approx_order,1.0,N,:None,:None)
+    Amat = full(A)
+
+    @test isapprox(Amat[1,1:4], [-11/6, 3, -3/2, 1/3])
+    for row = 3:N-2
+        @test isapprox(Amat[row,row-1:row+1], [-1/2, 0, 1/2])
+    end
+    @test isapprox(Amat[end,end-3:end], [-1/3, 3/2, -3, 11/6])
 end
