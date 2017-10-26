b9f5835aec97bfab2ccc17a5938a6ce10d454a02
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-25 20:21:01 -0700

    add second order check

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ tic()
 @time @testset "Dirichlet BCs" begin include("dirichlet.jl") end
 @time @testset "Periodic BCs" begin include("periodic.jl") end
 @time @testset "Neumann BCs" begin include("neumann.jl") end
+@time @testset "2nd order check" begin include("2nd_order_check.jl") end
 @time @testset "None BCs" begin include("none.jl") end
 @time @testset "KdV" begin include("KdV.jl") end # KdV times out
 @time @testset "Heat Equation" begin include("heat_eqn.jl") end
