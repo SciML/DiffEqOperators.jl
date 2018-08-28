fce87659e88a299cd3b3b2fb86adf9995cbc53ac
Xingjian Guo <xg703@nyu.edu>
2018-08-27 23:09:30 -0400

    Use the `xgrid` interface

--- src/derivative_operators/derivative_stencil.jl
+++ src/derivative_operators/derivative_stencil.jl
@@ -1,7 +1,6 @@
 struct UniformDerivativeStencil{T,S<:SVector} <: AbstractDiffEqLinearOperator{T}
     derivative_order    :: Int
     approximation_order :: Int
-    dx                  :: T
     dimension           :: Int
     stencil_length      :: Int
     stencil_coefs       :: S
@@ -9,26 +8,26 @@ struct UniformDerivativeStencil{T,S<:SVector} <: AbstractDiffEqLinearOperator{T}
         stencil_length = dorder + aorder - 1 + (dorder + aorder) % 2
         stl_2 = div(stencil_length, 2)
         stencil_coefs = convert(SVector{stencil_length,T}, calculate_weights(
-            dorder, zero(T), one(T) .* collect(-stl_2 : 1 : stl_2)))
-        new{T,typeof(stencil_coefs)}(dorder,aorder,dx,dim,stencil_length,stencil_coefs)
+            dorder, zero(T), dx .* collect(-stl_2 : 1 : stl_2)))
+        new{T,typeof(stencil_coefs)}(dorder,aorder,dim,stencil_length,stencil_coefs)
     end
+    UniformDerivativeStencil(xgrid::AbstractRange{T},dorder,aorder) where {T} =
+        UniformDerivativeStencil(dorder,aorder,step(xgrid),length(xgrid))
 end
 
 struct IrregularDerivativeStencil{T,S<:SVector} <: AbstractDiffEqLinearOperator{T}
     derivative_order    :: Int
     approximation_order :: Int
-    dx                  :: Vector{T}
     dimension           :: Int
     stencil_length      :: Int
     stencil_coefs       :: Vector{S}
-    function IrregularDerivativeStencil(dorder,aorder,dx::Vector{T},dim) where {T}
+    function IrregularDerivativeStencil(xgrid::Vector{T},dorder,aorder) where {T}
+        dim = length(xgrid)
         stencil_length = dorder + aorder - 1 + (dorder + aorder) % 2
         stl_2 = div(stencil_length, 2)
-        any(x -> x < zero(T), dx) && error("All grid steps must be greater than 0.0!")
-        x = [zero(T); cumsum(dx)]
         stencil_coefs = [convert(SVector{stencil_length, T}, calculate_weights(
-            dorder, zero(T), x[i-stl_2 : i+stl_2] .- x[i])) for i in stl_2+1:dim-stl_2]
-        new{T,eltype(stencil_coefs)}(dorder,aorder,dx,dim,stencil_length,stencil_coefs)
+            dorder, zero(T), xgrid[i-stl_2 : i+stl_2] .- xgrid[i])) for i in stl_2+1:dim-stl_2]
+        new{T,eltype(stencil_coefs)}(dorder,aorder,dim,stencil_length,stencil_coefs)
     end
 end
 
