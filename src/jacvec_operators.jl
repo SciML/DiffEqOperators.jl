bba423664903d5aed6f483fee52f32cd70bac143
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-04-20 09:00:07 -0400

    Generalize to J(f(x))*v

--- src/jacvec_operators.jl
+++ src/jacvec_operators.jl
@@ -3,6 +3,7 @@
 
 struct JacVecTag end
 
+# J(f(x))*v
 function jacvec!(du, f, x, v,
                  cache1 = ForwardDiff.Dual{JacVecTag}.(x, v),
                  cache2 = ForwardDiff.Dual{JacVecTag}.(x, v))
@@ -34,24 +35,25 @@ end
 
 ### Operator Implementation
 
-mutable struct JacVecOperator{T,F,T1,T2,P,tType} <: DiffEqBase.AbstractDiffEqLinearOperator{T}
+mutable struct JacVecOperator{T,F,T1,T2,uType,P,tType} <: DiffEqBase.AbstractDiffEqLinearOperator{T}
     f::F
     cache1::T1
     cache2::T2
+    u::uType
     p::P
     t::tType
 
     function JacVecOperator{T}(f,p=nothing,t::Union{Nothing,Number}=nothing) where T
         p===nothing ? P = Any : P = typeof(p)
         t===nothing ? tType = Any : tType = typeof(t)
-        new{T,typeof(f),Nothing,Nothing,P,tType}(f,nothing,nothing,nothing,nothing)
+        new{T,typeof(f),Nothing,Nothing,Any,P,tType}(f,nothing,nothing,nothing,nothing,nothing)
     end
     function JacVecOperator{T}(f,u::AbstractArray,p=nothing,t::Union{Nothing,Number}=nothing) where T
         cache1 = ForwardDiff.Dual{JacVecTag}.(u, u)
         cache2 = ForwardDiff.Dual{JacVecTag}.(u, u)
         p===nothing ? P = Any : P = typeof(p)
         t===nothing ? tType = Any : tType = typeof(t)
-        new{T,typeof(f),typeof(cache1),typeof(cache2),P,tType}(f,cache1,cache2,p,t)
+        new{T,typeof(f),typeof(cache1),typeof(cache2),typeof(u),P,tType}(f,cache1,cache2,u,p,t)
     end
     function JacVecOperator(f,u,args...)
         JacVecOperator{eltype(u)}(f,u,args...)
@@ -62,9 +64,12 @@ end
 Base.size(L::JacVecOperator) = (length(L.cache1),length(L.cache1))
 Base.size(L::JacVecOperator,i::Int) = length(L.cache1)
 function update_coefficients!(L::JacVecOperator,u,p,t)
+    L.u = u
     L.p = p
     L.t = t
 end
+
+# Interpret the call as df/du * u
 
 function (L::JacVecOperator)(u,p,t::Number)
     update_coefficients!(L,u,p,t)
@@ -76,14 +81,14 @@ function (L::JacVecOperator)(du,u,p,t::Number)
     mul!(du,L,u)
 end
 
-function Base.:*(L::JacVecOperator,u::AbstractVector) = jacvec(u->L.f(u,L.p,L.t),u,u)
+Base.:*(L::JacVecOperator,x::AbstractVector) = jacvec(_u->L.f(_u,L.p,L.t),L.u,x)
 
-function LinearAlgebra.mul!(du::AbstractVector,L::JacVecOperator,u::AbstractVector)
+function LinearAlgebra.mul!(du::AbstractVector,L::JacVecOperator,x::AbstractVector)
     let p=L.p,t=L.t
         if L.cache1 === nothing
-            jacvec!(du,(du,u)->L.f(du,u,p,t),u,u)
+            jacvec!(du,(_du,_u)->L.f(_du,_u,p,t),L.u,x)
         else
-            jacvec!(du,(du,u)->L.f(du,u,p,t),u,u,L.cache1,L.cache2)
+            jacvec!(du,(_du,_u)->L.f(_du,_u,p,t),L.u,x,L.cache1,L.cache2)
         end
     end
 end
