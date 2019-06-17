5d50e0612564f1f767485ebf2486b05d09fca593
Zander <alex.lemony@gmail.com>
2019-06-17 18:21:19 +0200

    3rd order robin and General test

--- test/robin.jl
+++ test/robin.jl
@@ -42,4 +42,19 @@ for i in 1:5
 
 end
 
+#3rd order RobinBC, calculated with left stencil [-11/6 3 -3/2 1/3], right stencil [-1/3 3/2 -3 11/6] and [α,β,γ] = [1 6 10]
+u0 = -4/10
+uend = -210/17
+u = Vector(1.0:10.0)
+Q = RobinBC([1.0, 6.0, 10.0], [1.0, 6.0, 10.0], [1.0,1.0], 3)
+urobinextended = Q*u
+@test urobinextended.l ≈ u0
+@test urobinextended.r ≈ uend
+# General BC should be equivalent
+G = GeneralBC([1.0, 6.0, 10.0], [1.0, 6.0, 10.0], [1.0,1.0], 3)
+ugeneralextended = G*u
+@test ugeneralextended.l ≈ u0
+@test ugeneralextended.r ≈ uend
+
+
 #TODO: Implement tests for BC's that are contingent on the sign of the coefficient on the operator near the boundary
