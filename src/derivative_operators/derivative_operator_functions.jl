178915367681d44ff41db3232914e35b97fc7389
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2019-06-29 14:25:02 -0700

    cleaned up typing of mul! and * overloads

--- src/derivative_operators/derivative_operator_functions.jl
+++ src/derivative_operators/derivative_operator_functions.jl
@@ -1,38 +1,40 @@
-function LinearAlgebra.mul!(x_temp::AbstractArray{T}, A::DerivativeOperator{T,N,Wind,T2,S1,S2,T3,F}, M::AbstractArray{T,MN}) where {T<:Real,N,Wind,T2,S1,S2<:SVector,T3,F,MN<:Int}
+for MT in [AbstractArray, AbstractMatrix]
+    @eval begin
+        function LinearAlgebra.mul!(x_temp::$MT{T}, A::DerivativeOperator{T,N}, M::$MT{T}) where {T<:Real,N}
 
-    v = zeros(ndims(x_temp))
-    v[N] = 2
-    @assert [size(x_temp)...]+v == [size(M)...]
+            v = zeros(ndims(x_temp))
+            v[N] = 2
+            @assert [size(x_temp)...]+v == [size(M)...]
 
-    ndimsM = ndims(M)
-    @assert N <= ndimsM
+            ndimsM = ndims(M)
+            @assert N <= ndimsM
 
-    dimsM = [axes(M)...]
-    alldims = [1:ndims(M);]
-    otherdims = setdiff(alldims, N)
+            dimsM = [axes(M)...]
+            alldims = [1:ndims(M);]
+            otherdims = setdiff(alldims, N)
 
-    idx = Any[first(ind) for ind in axes(M)]
-    itershape = tuple(dimsM[otherdims]...)
-    nidx = length(otherdims)
-    indices = Iterators.drop(CartesianIndices(itershape), 0)
+            idx = Any[first(ind) for ind in axes(M)]
+            itershape = tuple(dimsM[otherdims]...)
+            nidx = length(otherdims)
+            indices = Iterators.drop(CartesianIndices(itershape), 0)
 
-    setindex!(idx, :, N)
-    for I in indices
-        Base.replace_tuples!(nidx, idx, idx, otherdims, I)
-        mul!(view(x_temp, idx...), A, view(M, idx...))
+            setindex!(idx, :, N)
+            for I in indices
+                Base.replace_tuples!(nidx, idx, idx, otherdims, I)
+                mul!(view(x_temp, idx...), A, view(M, idx...))
+            end
+        end
     end
 end
 
-function *(A::DerivativeOperator{T,N,Wind,T2,S1,S2,T3,F},M::AbstractArray{T,MN}) where {T<:Real,N,Wind,T2,S1,S2<:SVector,T3,F,MN<:Int}
-    size_x_temp = [size(M)...]
-    size_x_temp[N] -= 2
-    x_temp = zeros(promote_type(eltype(A),eltype(M)), size_x_temp...)
-    LinearAlgebra.mul!(x_temp, A, M)
-    return x_temp
-end
-
-function *(A::DerivativeOperator{T,N,Wind,T2,S1,S2,T3,F},x::BoundaryPaddedVector) where {T<:Real,N,Wind,T2,S1,S2<:SVector,T3,F}
-    y = zeros(promote_type(eltype(A),eltype(x)), length(x)-2)
-    LinearAlgebra.mul!(y, A::AbstractDerivativeOperator, x::BoundaryPaddedVector)
-    return y
+for MT in [AbstractVector, AbstractMatrix, AbstractArray]
+    @eval begin
+        function wacky(A::DerivativeOperator{T,N},M::$MT{T}) where {T<:Real,N}
+            size_x_temp = [size(M)...]
+            size_x_temp[N] -= 2
+            x_temp = zeros(promote_type(eltype(A),eltype(M)), size_x_temp...)
+            LinearAlgebra.mul!(x_temp, A, M)
+            return x_temp
+        end
+    end
 end
