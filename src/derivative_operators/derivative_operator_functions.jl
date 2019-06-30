d1f82c49f1e677ea5c6b637cf251914cc9609ab6
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2019-06-29 19:53:18 -0700

    fixed redundancy in typing

--- src/derivative_operators/derivative_operator_functions.jl
+++ src/derivative_operators/derivative_operator_functions.jl
@@ -1,40 +1,32 @@
-for MT in [AbstractArray, AbstractMatrix]
-    @eval begin
-        function LinearAlgebra.mul!(x_temp::$MT{T}, A::DerivativeOperator{T,N}, M::$MT{T}) where {T<:Real,N}
+function LinearAlgebra.mul!(x_temp::AbstractArray{T}, A::DerivativeOperator{T,N}, M::AbstractArray{T}) where {T<:Real,N}
 
-            v = zeros(ndims(x_temp))
-            v[N] = 2
-            @assert [size(x_temp)...]+v == [size(M)...]
+    v = zeros(ndims(x_temp))
+    v[N] = 2
+    @assert [size(x_temp)...]+v == [size(M)...]
 
-            ndimsM = ndims(M)
-            @assert N <= ndimsM
+    ndimsM = ndims(M)
+    @assert N <= ndimsM
 
-            dimsM = [axes(M)...]
-            alldims = [1:ndims(M);]
-            otherdims = setdiff(alldims, N)
+    dimsM = [axes(M)...]
+    alldims = [1:ndims(M);]
+    otherdims = setdiff(alldims, N)
 
-            idx = Any[first(ind) for ind in axes(M)]
-            itershape = tuple(dimsM[otherdims]...)
-            nidx = length(otherdims)
-            indices = Iterators.drop(CartesianIndices(itershape), 0)
+    idx = Any[first(ind) for ind in axes(M)]
+    itershape = tuple(dimsM[otherdims]...)
+    nidx = length(otherdims)
+    indices = Iterators.drop(CartesianIndices(itershape), 0)
 
-            setindex!(idx, :, N)
-            for I in indices
-                Base.replace_tuples!(nidx, idx, idx, otherdims, I)
-                mul!(view(x_temp, idx...), A, view(M, idx...))
-            end
-        end
+    setindex!(idx, :, N)
+    for I in indices
+        Base.replace_tuples!(nidx, idx, idx, otherdims, I)
+        mul!(view(x_temp, idx...), A, view(M, idx...))
     end
 end
 
-for MT in [AbstractVector, AbstractMatrix, AbstractArray]
-    @eval begin
-        function *(A::DerivativeOperator{T,N},M::$MT{T}) where {T<:Real,N}
-            size_x_temp = [size(M)...]
-            size_x_temp[N] -= 2
-            x_temp = zeros(promote_type(eltype(A),eltype(M)), size_x_temp...)
-            LinearAlgebra.mul!(x_temp, A, M)
-            return x_temp
-        end
-    end
+function *(A::DerivativeOperator{T,N},M::AbstractArray{T}) where {T<:Real,N}
+    size_x_temp = [size(M)...]
+    size_x_temp[N] -= 2
+    x_temp = zeros(promote_type(eltype(A),eltype(M)), size_x_temp...)
+    LinearAlgebra.mul!(x_temp, A, M)
+    return x_temp
 end
