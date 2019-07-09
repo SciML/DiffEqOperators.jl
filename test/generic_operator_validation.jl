8d33fa624652263ace5b93ae057da3bbac789dcc
Zander <alex.lemony@gmail.com>
2019-07-09 13:04:19 +0200

    Changed Test for generic operator validation - FiniteDifference no longer exists

--- test/generic_operator_validation.jl
+++ test/generic_operator_validation.jl
@@ -9,34 +9,22 @@ y_im = exp.(π*im*x)
 yim_ = y_im[2:(end-1)]
 y_ = y[2:(end-1)]
 
-@test_broken for dor in 1:6, aor in 1:8
+for dor in 1:6, aor in 1:8
 
-    D1 = CenteredDifference(dor,aor,dx[1],length(x))
-    D2 = DiffEqOperators.FiniteDifference{Float64}(dor,aor,dx,length(x))
-    D = (D1,D2)
-    @test_broken convert(Array, D1) ≈ convert(Array, D2)
+    D1 = CenteredDifference(dor,aor,dx,length(x))
 
-    #take derivatives
+    #take derivative
     yprime1 = D1*y
-    yprime2 = D2*y
 
     #test result
-    @test_broken yprime1 ≈ (π^dor)*y_ # test operator with known derivative of exp(kx)
-    @test_broken yprime2 ≈ (π^dor)*y_ # test operator with known derivative of exp(kx)
-
-    #test equivalance
-    @test_broken yprime1 ≈ yprime2
+    @test yprime1 ≈ (π^dor)*y_ # test operator with known derivative of exp(kx)
 
     #take derivatives
     y_imprime1 = D1*y_im
-    y_imprime2 = D2*y_im
 
     #test result
     @test_broken y_imprime1 ≈ ((pi*im)^dor)*yim_ # test operator with known derivative of exp(jkx)
-    @test_broken y_imprime2 ≈ ((pi*im)^dor)*yim_ # test operator with known derivative of exp(jkx)
 
-    #test equivalance
-    @test_broken y_imprime1 ≈ y_imprime2
 
     #TODO: implement specific tests for the left and right boundary regions, waiting until after update
 end
