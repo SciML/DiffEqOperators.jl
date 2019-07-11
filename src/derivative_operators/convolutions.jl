fbfd73821c02f5127f2a41d996e3b2ae6f07ad0d
Zander <alex.lemony@gmail.com>
2019-07-11 20:54:48 +0200

    first draft of a curl operator convolution

--- src/derivative_operators/convolutions.jl
+++ src/derivative_operators/convolutions.jl
@@ -133,3 +133,30 @@ function convolve_BC_right!(x_temp::AbstractVector{T}, _x::BoundaryPaddedVector,
         x_temp[bc_start + i] = xtempi
     end
 end
+
+################################################################################
+# Stub for curl operator convolution draft while its still in my head
+################################################################################
+
+#referring to https://en.wikipedia.org/wiki/Curl_(mathematics)
+# h[i] = √((Δᵤ[i]*x)^2 + (Δᵤ[i]*y)^2 + (Δᵤ[i]*z)^2)) where x,y,z are cartesian coords and u[1], u[2], u[3] is the orthogonal coord system in use
+function convolve_interior!(x_temp::AbstractArray{SVector{3, T}, 3}, u::AbstractArray{SVector{3, T},3}, A::CurlOperator)
+    s = size(x_temp)
+    stencil = A.stencil_coefs
+    coeff   = A.coefficients
+    interior = [(2+A.boundary_point_count) : s[i]-A.boundary_point_count)-1 for i in 1:3]
+    mid = div(A.stencil_length,2)
+
+    for i in interior[1], j in interior[2], k in interior[3]
+        x̄ = zeros(T, 3)
+        cur_stencil = eltype(stencil) <: AbstractVector ? stencil[i] : stencil
+        cur_coeff   = typeof(coeff)   <: AbstractVector ? coeff[i] : true
+        cur_stencil = use_winding(A) && cur_coeff < 0 ? reverse(cur_stencil) : cur_stencil
+        for idx in (1-mid):(A.stencil_length-mid)
+            x̄[1] += cur_coeff*cur_stencil[idx]*(A.h[i, j+idx, k]*u[i, j+idx, k][3] - A.h[i, j, k+idx]*u[i, j, k+idx][2])/(A.h[i, j+idx, k]*A.h[i, j, k+idx])
+            x̄[2] += cur_coeff*cur_stencil[idx]*(A.h[i, j, k+idx]*u[i, j, k+idx][1] - A.h[i+idx, j, k]*u[i+idx, j, k][3])/(A.h[i, j, k+idx]*A.h[i+idx, j, k])
+            x̄[3] += cur_coeff*cur_stencil[idx]*(A.h[i+idx, j, k]*u[i+idx, j, k][2] - A.h[i, j+idx, k]*u[i, j+idx, k][1])/(A.h[i+idx, j, k]*A.h[i, j+idx, k])
+        end
+        x_temp[i,j,k] = SVector(x̄)
+    end
+end
