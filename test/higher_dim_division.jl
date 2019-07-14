67ac0983ab73ab143a5bfed4ffdfa382e3854d85
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2019-07-13 23:08:59 -0700

    tests added for 2d case

--- test/higher_dim_division.jl
+++ test/higher_dim_division.jl
@@ -2,11 +2,51 @@ using SparseArrays, DiffEqOperators, LinearAlgebra, Random,
       Test, BandedMatrices, FillArrays, LazyArrays
 
 
+# The following test checks that left division in the second dimension is consistent
+# with applying left division at each slice in the second dimension.
+@testset "Basic Second Dimension Division" begin
+
+    # The second dimension is the last dimension of the array
+    A = zeros(5,7)
+    for i in 1:5
+        for j in 1:7
+            A[i,j] = sin(0.1i+j)
+        end
+    end
+    L = CenteredDifference{2}(1,3, 1.0, 5)
+    L1 = CenteredDifference{1}(1,3, 1.0, 5)
+    B = L*A
+
+    C = L\B
+    for i in 1:5
+        @test C[i,:] ≈ L1 \ B[i,:]
+    end
+
+    # The second dimension is the second last dimension of the array
+    A = zeros(5,7,5)
+    for i in 1:5
+        for j in 1:7
+            for k in 1:5
+                A[i,j,k] = sin(0.1i+0.3k+j)
+            end
+        end
+    end
+    B = L*A
+
+    C = L\B
+    for i in 1:5
+        for k in 1:5
+            @test C[i,:,k] ≈ L1 \ B[i,:,k]
+        end
+    end
+
+end
+
+
+
 # The following test checks that left division in the third dimension is consistent
 # with applying left division at each slice in the third dimension.
 @testset "Basic Third Dimension Division" begin
-
-
     A = zeros(2,2,7)
     for i in 1:2
         for j in 1:2
@@ -26,6 +66,4 @@ using SparseArrays, DiffEqOperators, LinearAlgebra, Random,
             @test C[i,j,:] ≈ L1 \ B[i,j,:]
         end
     end
-
-
 end
