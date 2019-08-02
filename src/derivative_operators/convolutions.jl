6cba4931f846dd09039f834a30e2f2b9eba85dff
BuildTools <alex.lemony@gmail.com>
2019-08-02 15:14:48 +0200

    CartesianIndex everywhere!

--- src/derivative_operators/convolutions.jl
+++ src/derivative_operators/convolutions.jl
@@ -144,18 +144,27 @@ function convolve_interior!(x_temp::AbstractArray{SVector{3, T}, 3}, u::Abstract
     s = size(x_temp)
     stencil = A.stencil_coefs
     coeff   = A.coefficients
-    interior = [(2+A.boundary_point_count) : s[i]-A.boundary_point_count)-1 for i in 1:3]
+    R = CartesianIndices((2+A.boundary_point_count) : (s[i]-A.boundary_point_count-3) for i in 1:3)
+    ê = begin #create unit CartesianIndex for each dimension
+        out = Vector{CartesianIndex{N}}(undef, 3)
+        null = zeros(Int64, 3)
+        for i in 1:3
+            unit_i = copy(null)
+            unit_i[i] = 1
+            out[i] = CartesianIndex(Tuple(unit_i))
+        end
+        out
+    end
     mid = div(A.stencil_length,2)
-
-    for i in interior[1], j in interior[2], k in interior[3]
-        x̄ = zeros(T, 3)
-        cur_stencil = eltype(stencil) <: AbstractVector ? stencil[i] : stencil
-        cur_coeff   = typeof(coeff)   <: AbstractVector ? coeff[i] : true
+    for I in R
+        x̄ = zeros(T,3)
+        cur_stencil = eltype(stencil) <: AbstractArray ? stencil[I] : stencil
+        cur_coeff   = typeof(coeff)   <: AbstractArray ? coeff[I] : true
         cur_stencil = use_winding(A) && cur_coeff < 0 ? reverse(cur_stencil) : cur_stencil
         for idx in (1-mid):(A.stencil_length-mid)
-            x̄[1] += cur_coeff*cur_stencil[idx]*(A.h[i, j-idx, k]*u[i, j-idx, k][3] - A.h[i, j, k-idx]*u[i, j, k-idx][2])/(A.h[i, j-idx, k]*A.h[i, j, k-idx])
-            x̄[2] += cur_coeff*cur_stencil[idx]*(A.h[i, j, k-idx]*u[i, j, k-idx][1] - A.h[i-idx, j, k]*u[i-idx, j, k][3])/(A.h[i, j, k-idx]*A.h[i-idx, j, k])
-            x̄[3] += cur_coeff*cur_stencil[idx]*(A.h[i-idx, j, k]*u[i-idx, j, k][2] - A.h[i, j-idx, k]*u[i, j-idx, k][1])/(A.h[i-idx, j, k]*A.h[i, j-idx, k])
+            x̄[1] += cur_coeff*cur_stencil[idx]*(A.h[I-ê[2]*idx] * u[I-ê[2]*idx][3] - A.h[I-ê[2]*idx] * u[I-ê[3]*idx][2])/(A.h[I-ê[2]*idx]*A.h[I-ê[3]*idx])
+            x̄[2] += cur_coeff*cur_stencil[idx]*(A.h[I-ê[3]*idx] * u[I-ê[3]*idx][1] - A.h[I-ê[1]*idx] * u[I-ê[1]*idx][3])/(A.h[I-ê[3]*idx]*A.h[I-ê[1]*idx])
+            x̄[3] += cur_coeff*cur_stencil[idx]*(A.h[I-ê[1]*idx] * u[I-ê[1]*idx][2] - A.h[I-ê[2]*idx] * u[I-ê[2]*idx][1])/(A.h[I-ê[1]*idx]*A.h[I-ê[2]*idx])
         end
         x_temp[i,j,k] = SVector(x̄)
     end
