fdaca9e3e3a1a79893c67bf19e1be4d0ff64ad83
BuildTools <alex.lemony@gmail.com>
2019-08-04 20:38:25 +0200

    "

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,6 @@ import Base: isapprox
 @time @safetestset "Basic Operators Interface" begin include("basic_operators_interface.jl") end
 @time @safetestset "Robin Boundary Condition Operators" begin include("robin.jl") end
 @time @safetestset "JacVec Operators Interface" begin include("jacvec_operators.jl") end
-@time @safetestset "Validate BridgeBC and its constructors" begin include("bridge_bc.jl") end
 @time @safetestset "Composite Operators Interface" begin include("composite_operators_interface.jl") end
 @time @safetestset "BC and Coefficient Compositions" begin include("bc_coeff_compositions.jl") end
 @time @safetestset "Derivative Operators Interface" begin include("derivative_operators_interface.jl") end
@@ -17,3 +16,4 @@ import Base: isapprox
 @time @safetestset "Matrix-Free Operators" begin include("matrixfree.jl") end
 @time @safetestset "Convolutions" begin include("convolutions.jl") end
 @time @safetestset "Differentiation Dimension" begin include("differentiation_dimension.jl") end
+ 
