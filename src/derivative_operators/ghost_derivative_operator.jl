b0ec7369c6ad1acc5692b5ae34b43d5e0158f716
BuildTools <alex.lemony@gmail.com>
2019-09-03 17:07:34 +0200

    removed assumptions that `L` is a derivative operator, made `GhostDerivativeOperator` an `AbstractDiffEqLinearOperator`

--- src/derivative_operators/ghost_derivative_operator.jl
+++ src/derivative_operators/ghost_derivative_operator.jl
@@ -12,45 +12,24 @@ function *(L::AbstractDiffEqCompositeOperator{T}, Q::AbstractBC{T}) where{T}
 end
 
 function LinearAlgebra.mul!(x::AbstractArray{T,N}, A::GhostDerivativeOperator{T,E,F}, u::AbstractArray{T,N}) where {T,E,F,N}
-    @assert size(u) == size(x)
     LinearAlgebra.mul!(x, A.L, u)
 end
 
 function *(A::GhostDerivativeOperator{T1}, u::AbstractVector{T2}) where {T1,T2}
-    @assert length(u) == A.L.len
+    #TODO Implement a function domaincheck(L::AbstractDiffEqLinearOperator, u) to see if components of L along each dimension match the size of u
     x = similar(u, promote_type(T1,T2))
     LinearAlgebra.mul!(x, A, A.Q*u)
     return x
 end
 
-<<<<<<< HEAD
-function LinearAlgebra.mul!(M_temp::AbstractMatrix, A::GhostDerivativeOperator, M::AbstractMatrix)
-    @assert size(M,1) == size(M_temp,1) == A.L.len
-    for i in 1:size(M,2)
-        mul!(view(M_temp,:,i), A, view(M,:,i))
-    end
-end
-
-function *(A::GhostDerivativeOperator{T1}, M::AbstractMatrix{T2}) where {T1,T2}
-    @assert size(M,1) == A.L.len
-    M_temp = zeros(promote_type(T1,T2), A.L.len, size(M,2))
-    LinearAlgebra.mul!(M_temp, A, M)
-    return M_temp
-end
-
-
-function LinearAlgebra.ldiv!(x::AbstractVector, A::GhostDerivativeOperator, u::AbstractVector)
-=======
 function LinearAlgebra.ldiv!(x::AbstractVector{T}, A::GhostDerivativeOperator{T,E,F}, u::AbstractVector{T}) where {T,E,F}
->>>>>>> f00c2c5... Allowed AbstractArrays instead of AbstractVecOrMat, allowed AbstractArrays for GhostDerivativeOperator
-    @assert length(x) == A.L.len
+    @assert length(x) == size(A.L,2)
     (AL,Ab) = Array(A)
     LinearAlgebra.ldiv!(x, lu!(AL), u-Ab)
 end
 
 
 function \(A::GhostDerivativeOperator{T1}, u::AbstractVector{T2}) where {T1,T2}
-    @assert length(u) == A.L.len
     x = zeros(promote_type(T1,T2),size(A,2))
     LinearAlgebra.ldiv!(x, A, u)
     return x
@@ -91,10 +70,8 @@ function *(coeff_func::Function, A::GhostDerivativeOperator)
     (coeff_func*A.L)*A.Q
 end
 
-=======
->>>>>>> f00c2c5... Allowed AbstractArrays instead of AbstractVecOrMat, allowed AbstractArrays for GhostDerivativeOperator
 # length and sizes
 Base.ndims(A::GhostDerivativeOperator) = 2
-Base.size(A::GhostDerivativeOperator) = (A.L.len, A.L.len)
+Base.size(A::GhostDerivativeOperator) = (size(A.L, 2), size(A.L, 2))
 Base.size(A::GhostDerivativeOperator,i::Integer) = size(A)[i]
 Base.length(A::GhostDerivativeOperator) = reduce(*, size(A))
