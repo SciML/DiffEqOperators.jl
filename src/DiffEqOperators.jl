d9eaf2924b1f595bfa28101ee7374148feec752f
BuildTools <alex.lemony@gmail.com>
2019-09-05 20:24:45 +0200

    add a Laplacian constructor, extend the f(u,p,t) interface to GhostDerivativeOperator

--- src/DiffEqOperators.jl
+++ src/DiffEqOperators.jl
@@ -45,7 +45,7 @@ include("docstrings.jl")
 include("derivative_operators/concretization.jl")
 
 # The (u,p,t) and (du,u,p,t) interface
-for T in [DiffEqScaledOperator, DiffEqOperatorCombination, DiffEqOperatorComposition]
+for T in [DiffEqScaledOperator, DiffEqOperatorCombination, DiffEqOperatorComposition, GhostDerivativeOperator]
   (L::T)(u,p,t) = (update_coefficients!(L,u,p,t); L * u)
   (L::T)(du,u,p,t) = (update_coefficients!(L,u,p,t); mul!(du,L,u))
 end
