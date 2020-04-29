616e37846de3d6cc3673fb46353bc0e2d6f66f8b
Dan <dan.girsh@gmail.com>
2020-04-29 13:17:22 -0700

    update 2nd_order_check to most recent interface. Tests pass.

--- test/2nd_order_check.jl
+++ test/2nd_order_check.jl
@@ -2,24 +2,30 @@ using DiffEqOperators, Test, LinearAlgebra
 
 @testset "Second Order Value Check" begin
     ord = 2               # Order of approximation
-    Δx = 0.025π
-    N = Int(2*(π/Δx)) -2
-    x = -π:Δx:π-Δx
-    D1  = CenteredDifference(1,ord,Δx,N,:periodic,:periodic);    # 1nd Derivative
-    D2  = CenteredDifference(2,ord,Δx,N,:periodic,:periodic);    # 2nd Derivative
+    h = 0.025π
+    N = Int(2*(π/h))
+    x = -π:h:π-h
+    L1  = CenteredDifference(1,ord,h,N);
+    L2 = CenteredDifference(2,ord,h,N);
+
+    Q = PeriodicBC(Float64)
 
     u0 = cos.(x)
     du_true = -cos.(x)
 
-    M = Matrix(Tridiagonal([1.0 for i in 1:N+1],[-2.0 for i in 1:N+2],[1.0 for i in 1:N+1]))
-    # Do the reflections, different for x and y operators
+    # Explicit stencil for L2
+    M = Matrix(Tridiagonal([1.0 for i in 1:N-1],
+                           [-2.0 for i in 1:N],
+                           [1.0 for i in 1:N-1]))
+
+    # Insert periodic BC
     M[end,1] = 1.0
     M[1,end] = 1.0
-    M = M/(Δx^2)
-    @test M*u0 ≈ D2*u0
+    M = M/(h^2)
+    @test M*u0 ≈ L2*Q*u0
 
     A = zeros(size(M))
-    for i in 1:N+2, j in 1:N+2
+    for i in 1:N, j in 1:N
         i == j && (A[i,j] = -0.5)
         abs(i - j) == 2 && (A[i,j] = 0.25)
     end
@@ -27,17 +33,17 @@ using DiffEqOperators, Test, LinearAlgebra
     A[end,2] = 0.25
     A[1,end-1] = 0.25
     A[2,end] = 0.25
-    A = A/(Δx^2)
+    A = A/(h^2)
 
-    @test A*u0 ≈ D1*(D1*u0)
+    @test A*u0 ≈ L1*Q*(L1*Q*u0)
 
     κ(x) = 1.0
     tmp1 = similar(x)
     tmp2 = similar(x)
     function f(t,u,du)
-        mul!(tmp1,  D1, u)
+        mul!(tmp1,L1,Q*u)
         @. tmp2 = κ(x)*tmp1
-        mul!(du,D1,tmp2)
+        mul!(du,L1,Q*tmp2)
     end
 
     du = similar(u0)
@@ -45,7 +51,7 @@ using DiffEqOperators, Test, LinearAlgebra
     du ≈ A*u0
 
     error = du_true - du
-    du2 = D2*u0
+    du2 = L2*Q*u0
     error2 = du_true - du2
 
     @test maximum(error) < 0.004
