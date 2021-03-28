5b9095add0a188939bac7669bb96670788bd1d57
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-03-27 21:48:00 -0400

    add 2D heat

--- test/MOLtest.jl
+++ test/MOLtest.jl
@@ -1,4 +1,4 @@
-using ModelingToolkit, DiffEqOperators, LinearAlgebra
+using ModelingToolkit, DiffEqOperators, LinearAlgebra, OrdinaryDiffEq
 
 # Define some variables
 @parameters t x
@@ -15,6 +15,39 @@ domains = [t ∈ IntervalDomain(0.0,1.0),
 pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
 discretization = MOLFiniteDifference([x=>0.1],t)
 prob = discretize(pdesys,discretization) # This gives an ODEProblem since it's time-dependent
+sol = solve(prob,Tsit5())
 
-using OrdinaryDiffEq
+@parameters t x y
+@variables u(..)
+Dxx = Differential(x)^2
+Dyy = Differential(y)^2
+Dt = Differential(t)
+t_min= 0.
+t_max = 2.0
+x_min = 0.
+x_max = 2.
+y_min = 0.
+y_max = 2.
+
+# 3D PDE
+eq  = Dt(u(t,x,y)) ~ Dxx(u(t,x,y)) + Dyy(u(t,x,y))
+
+analytic_sol_func(t,x,y) = exp(x+y)*cos(x+y+4t)
+# Initial and boundary conditions
+bcs = [u(t_min,x,y) ~ analytic_sol_func(t_min,x,y),
+       u(t,x_min,y) ~ analytic_sol_func(t,x_min,y),
+       u(t,x_max,y) ~ analytic_sol_func(t,x_max,y),
+       u(t,x,y_min) ~ analytic_sol_func(t,x,y_min),
+       u(t,x,y_max) ~ analytic_sol_func(t,x,y_max)]
+
+# Space and time domains
+domains = [t ∈ IntervalDomain(t_min,t_max),
+           x ∈ IntervalDomain(x_min,x_max),
+           y ∈ IntervalDomain(y_min,y_max)]
+pdesys = PDESystem([eq],bcs,domains,[t,x,y],[u(t,x,y)])
+
+# Method of lines discretization
+dx = 0.1; dy = 0.2
+discretization = MOLFiniteDifference([x=>dx,y=>dy],t)
+prob = ModelingToolkit.discretize(pdesys,discretization)
 sol = solve(prob,Tsit5())
