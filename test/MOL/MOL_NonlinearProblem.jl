ec42b80d148daee765ca3a0caaafa45592454101
Akash Garg <git@akashgarg.org>
2021-06-09 16:05:25 -0400

    normalizing equations for NonlinearProblem + tests.

--- test/MOL/MOL_NonlinearProblem.jl
+++ test/MOL/MOL_NonlinearProblem.jl
@@ -5,14 +5,29 @@ using ModelingToolkit,DiffEqOperators,LinearAlgebra,Test,OrdinaryDiffEq
 using ModelingToolkit: Differential
 using DifferentialEquations
 
-# Laplace's Equation
-@test_broken begin
+
+# Handrolled discrete laplace problem
+@test begin
+    @variables a b c d
+    eqs = [0 ~ a - 1,
+           0 ~ a + c - 2*b,
+           0 ~ b + d - 2*c,
+           0 ~ d - 1]
+    ns = NonlinearSystem(eqs, [a, b, c, d], [])
+    f = eval(generate_function(ns, [a, b, c, d])[2])
+    prob = NonlinearProblem(ns, zeros(4), [])
+    sol = solve(prob)
+    @test sol.u ≈ ones(4)
+end
+
+# Laplace's Equation, same as above but with MOL discretization
+@test begin
     @parameters x
     @variables u(..)
     Dxx = Differential(x)^2
 
     eq = Dxx(u(x)) ~ 0
-    dx = 0.1
+    dx = 1/3
 
     bcs = [u(0) ~ 1,
            u(1) ~ 1]
@@ -24,5 +39,29 @@ using DifferentialEquations
     discretization = MOLFiniteDifference([x=>dx], nothing, centered_order=2)
     prob = discretize(pdesys,discretization)
     sol = solve(prob)
+
+    @test sol.u ≈ ones(4)
 end
 
+# Laplace's Equation, linear solution
+@test begin
+    @parameters x
+    @variables u(..)
+    Dxx = Differential(x)^2
+
+    eq = Dxx(u(x)) ~ 0
+    dx = 0.1
+
+    bcs = [u(0) ~ 1,
+           u(1) ~ 2]
+
+    # Space and time domains
+    domains = [x ∈ IntervalDomain(0.0,1.0)]
+
+    pdesys = PDESystem([eq],bcs,domains,[x],[u(x)])
+    discretization = MOLFiniteDifference([x=>dx], nothing, centered_order=2)
+    prob = discretize(pdesys,discretization)
+    sol = solve(prob)
+
+    @test sol.u ≈ 1.0:0.1:2.0
+end
