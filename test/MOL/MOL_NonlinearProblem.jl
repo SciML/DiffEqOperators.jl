d7372dcd05622a573eee90abe8b5524d1e6632bd
Akash Garg <git@akashgarg.org>
2021-06-14 15:54:41 -0400

    updating tests for nonlinear problem

--- test/MOL/MOL_NonlinearProblem.jl
+++ test/MOL/MOL_NonlinearProblem.jl
@@ -4,10 +4,11 @@
 using ModelingToolkit,DiffEqOperators,LinearAlgebra,Test,OrdinaryDiffEq
 using ModelingToolkit: Differential
 using DifferentialEquations
+using DomainSets
 
 
 # Handrolled discrete laplace problem
-@test begin
+@testset "Hand rolled 1D Laplace" begin
     @variables a b c d
     eqs = [0 ~ a - 1,
            0 ~ a + c - 2*b,
@@ -21,7 +22,7 @@ using DifferentialEquations
 end
 
 # Laplace's Equation, same as above but with MOL discretization
-@test begin
+@testset "1D Laplace - constant solution" begin
     @parameters x
     @variables u(..)
     Dxx = Differential(x)^2
@@ -33,7 +34,7 @@ end
            u(1) ~ 1]
 
     # Space and time domains
-    domains = [x ∈ IntervalDomain(0.0,1.0)]
+    domains = [x ∈ Interval(0.0,1.0)]
 
     pdesys = PDESystem([eq],bcs,domains,[x],[u(x)])
     discretization = MOLFiniteDifference([x=>dx], nothing, centered_order=2)
@@ -44,7 +45,7 @@ end
 end
 
 # Laplace's Equation, linear solution
-@test begin
+@testset "1D Laplace - linear solution" begin
     @parameters x
     @variables u(..)
     Dxx = Differential(x)^2
@@ -56,7 +57,7 @@ end
            u(1) ~ 2]
 
     # Space and time domains
-    domains = [x ∈ IntervalDomain(0.0,1.0)]
+    domains = [x ∈ Interval(0.0,1.0)]
 
     pdesys = PDESystem([eq],bcs,domains,[x],[u(x)])
     discretization = MOLFiniteDifference([x=>dx], nothing, centered_order=2)
@@ -64,4 +65,50 @@ end
     sol = solve(prob)
 
     @test sol.u ≈ 1.0:0.1:2.0
+end
+
+# 2D heat
+@testset "2D heat equation" begin
+    @parameters x y
+    @variables u(..)
+    Dxx = Differential(x)^2
+    Dyy = Differential(y)^2
+
+    eq = Dxx(u(x, y)) + Dyy(u(x, y))~ 0
+    dx = 0.1
+    dy = 0.1
+
+    bcs = [u(0,y) ~ x*y,
+           u(1,y) ~ x*y,
+           u(x,0) ~ x*y,
+           u(x,1) ~ x*y]
+
+
+    # Space and time domains
+    domains = [x ∈ Interval(0.0,1.0),
+               y ∈ Interval(0.0,1.0)]
+
+    pdesys = PDESystem([eq],bcs,domains,[x,y],[u(x,y)])
+
+    # Note that we pass in `nothing` for the time variable `t` here since we
+    # are creating a stationary problem without a dependence on time, only space.
+    discretization = MOLFiniteDifference([x=>dx,y=>dy], nothing, centered_order=2)
+
+    prob = discretize(pdesys,discretization)
+    sol = solve(prob)
+    xs,ys = [infimum(d.domain):dx:supremum(d.domain) for d in domains]
+    u_sol = reshape(sol.u, (length(xs),length(ys)))
+
+    # test boundary
+    @test all(u_sol[:,1] .< eps())
+    @test all(u_sol[1,:] .< eps())
+    @test u_sol[:,end] ≈ 0:dy:1.0
+    @test u_sol[end,:] ≈ 0:dx:1.0
+
+    # test interior with finite differences
+    interior = CartesianIndices((axes(xs)[1], axes(ys)[1]))[2:end-1,2:end-1]
+    fd = map(interior) do I
+        u_sol[(I - CartesianIndex(1, 0))] + u_sol[(I + CartesianIndex(1,0))] - 2*u_sol[I] < eps(Float32)
+    end
+    @test all(fd)
 end
